2a4d943cff193036c34539cc7d54405a669067e2
Raf Guns <raf.guns@uantwerpen.be>
2021-01-25 21:21:36 +0100

    Add docstrings

--- textual_coherence.py
+++ textual_coherence.py
@@ -1,3 +1,11 @@
+"""
+This is an implementation of textual coherence for the evaluation of cluster quality,
+as previously used by Boyack and colleagues (`study 1`_, `study 2`_).
+
+.. _study 1: https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0018029#s2
+.. _study 2: https://onlinelibrary.wiley.com/doi/pdf/10.1002/asi.21419
+
+"""
 from typing import List, Tuple
 
 import numpy as np
@@ -6,11 +14,20 @@ from tqdm.auto import trange
 
 
 def jensen_shannon_divergence(p: np.ndarray, q: np.ndarray) -> float:
+    """Calculate Jensen-Shannon divergence between vectors p and q"""
     # jensenshannon returns sqrt of J-S divergence
     return jensenshannon(p, q) ** 2
 
 
 def cluster_jsd_value(doc_probs: np.ndarray, cluster_prob: np.ndarray) -> float:
+    """Calculate JSD value for cluster
+
+    Here, `doc_probs` is an n × m array (n documents, m words), where each element
+    gives the probability of a word in a document. `cluster_prob` is a vector of
+    length m, giving the probability of a word in the whole cluster. Use function
+    `probabilities` to calculate these for a document-word array.
+
+    """
     doc_jsd_values = np.apply_along_axis(
         jensen_shannon_divergence, 1, doc_probs, cluster_prob
     )
@@ -18,6 +35,14 @@ def cluster_jsd_value(doc_probs: np.ndarray, cluster_prob: np.ndarray) -> float:
 
 
 def probabilities(docs: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    """Document probabilities and cluster probability of words in array `docs`
+
+    Given a document-word array, this calculates two probabilities:
+    * the probability of a word in a document as an n × m array (n documents, m
+      words)
+    * the probability of a word in the whole cluster as a vector of length m
+
+    """
     doc_probs = docs / docs.sum(axis=1, keepdims=True)
     cluster_prob = docs.sum(axis=0) / docs.sum()
 
@@ -25,6 +50,7 @@ def probabilities(docs: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
 
 
 def random_cluster(all_docs: np.ndarray, cluster_size: int) -> np.ndarray:
+    """Get a random cluster of given size"""
     indexes = np.random.choice(len(all_docs), cluster_size)
     return all_docs[indexes]
 
@@ -32,6 +58,7 @@ def random_cluster(all_docs: np.ndarray, cluster_size: int) -> np.ndarray:
 def jsd_samples(
     all_docs: np.ndarray, cluster_size: int, num_samples: int = 5000
 ) -> np.ndarray:
+    """Determine JSD for random clusters of given size"""
     samples = np.empty(num_samples)
 
     for i in trange(num_samples):
@@ -45,6 +72,14 @@ def jsd_samples(
 def single_cluster_coherence(
     clu: np.ndarray, all_docs: np.ndarray, num_samples: int = 5000
 ) -> float:
+    """Normalized coherence of a single cluster `clu`
+
+    Coherence is affected by cluster size, and needs to be normalized.
+    Normalization happens by comparing the coherence of `clu` to that of random
+    clusters of similar size. More specifically, we calculate the average JSD
+    value for `num_samples` random clusters of the same size.
+
+    """
     # Calculate actual JSD value for cluster
     doc_probs, cluster_prob = probabilities(clu)
     jsd_actual = cluster_jsd_value(doc_probs, cluster_prob)
@@ -56,6 +91,17 @@ def single_cluster_coherence(
 
 
 def coherence(clustering_solution: List[np.ndarray], num_samples: int = 5000) -> float:
+    r"""Average coherence for entire cluster solution
+
+    This is calculated as a weighted average of coherences over all clusters
+
+    .. math::
+
+        Coh = \frac{\sum{n_i Coh_i}}{\sum{n_i}}
+
+    where $n_i$ is the size of cluster $i$.
+
+    """
     # Basic checks to make sure clustering_solution is in the right format
     for clu in clustering_solution:
         if len(clu.shape) != 2:
